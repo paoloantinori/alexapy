1a496c0e635e8211e73a27f5f8b242bf89d2d2e7
Alan D. Tse <alandtse@gmail.com>
2021-01-05 15:10:53 -0800

    fix: accept .domain cookies This uses self._cookies which will be deprecated but is necessary for now.

--- alexapy/alexalogin.py
+++ alexapy/alexalogin.py
@@ -475,7 +475,7 @@ class AlexaLogin:
             )
         for cookie in cookie_jar:
             oldvalue = self._cookies[cookie.key] if cookie.key in self._cookies else ""
-            if cookie["domain"] == str(site) or cookie["domain"] == "":
+            if cookie["domain"] in [str(site), f".{site}", ""]:
                 self._cookies[cookie.key] = cookie.value
                 if self._debug and oldvalue != cookie.value:
                     _LOGGER.debug(
